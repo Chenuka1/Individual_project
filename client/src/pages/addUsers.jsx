e8bfe016b3893c608b9e2a503da59ddce73f646f
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2024-01-31 20:47:38 +0530

    few changes

--- client/src/pages/addUsers.jsx
+++ client/src/pages/addUsers.jsx
@@ -3,8 +3,40 @@ import React from "react";
 function AddUsers() {
   return (
     <div>
-      <h1>Register new patients</h1>
-      {/* Add your component content here */}
+      <h1>Add new patient</h1>
+      <form>
+        <label>Enter the full name</label>
+        <input type="text" placeholder="Enter full name"/><br></br>
+        <label>Enter the birthyear</label>
+        <input type="date" placeholder="Enter birthdate"/><br></br>
+        <label>Enter gender</label>
+        <label>
+            <input type="radio" name="gender" value="male"/>
+            Male
+        </label>
+        <label>
+            <input type="radio" name="gender" value="female"/>
+            Female
+        </label>
+        <br></br>
+        <label>Enter the Blood type</label>
+        <input type="text" placeholder="Enter blood type" /><br></br>
+        
+
+
+        
+
+        
+
+        
+
+
+
+
+
+
+      </form>
+      
     </div>
   );
 }

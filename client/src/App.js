a69d920c4c0d66709f8f47d4b60d66fd3ae26cce
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2024-01-31 12:24:48 +0530

    correct code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
-
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import patientManagement from "./pages/patientManagement";
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddUsers from './pages/addUsers';
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<patientManagement/>}/>
-
+          <Route path="/" element={<AddUsers/>} />
+          {/* Add more Route components for other pages if needed */}
         </Routes>
-      
       </BrowserRouter>
-      
     </div>
   );
 }
--- client/src/pages/addUsers.jsx
+++ client/src/pages/addUsers.jsx
@@ -1,18 +1,12 @@
-import react from react;
+import React from "react";
 
-export default function addUsers(){
-
-    return(
-
-        <div>
-
-            <h1>Register new patients</h1>
-
-
-
-
-
-
-        </div>
-    )
+function AddUsers() {
+  return (
+    <div>
+      <h1>Register new patients</h1>
+      {/* Add your component content here */}
+    </div>
+  );
 }
+
+export default AddUsers;

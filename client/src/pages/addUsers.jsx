54c48bcb9c0aa0db245630abffa36f2901f78e99
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2024-02-01 12:41:53 +0530

    Update addUsers.jsx

--- client/src/pages/addUsers.jsx
+++ client/src/pages/addUsers.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 function AddUsers() {
   return (
-    <div>
-      <h1>Add new patient</h1>
+    <div className="form">
+      <h1>Register new patients</h1>
       <form>
-        <label>Enter the full name</label>
-        <input type="text" placeholder="Enter full name"/><br></br>
-        <label>Enter the birthyear</label>
+        <label>Enter birth certificate ID</label>
+        <input type="text" placeholder="Birth certificate id" /><br></br>
+        <label>Enter the first name</label>
+        <input type="text" placeholder="Enter first name"/><br></br>
+        <label>Enter the last name</label>
+        <input type="text" placeholder="Enter last name"/><br></br>
+        <label>Enter the birthdate</label>
         <input type="date" placeholder="Enter birthdate"/><br></br>
         <label>Enter gender</label>
         <label>
@@ -19,22 +23,23 @@ function AddUsers() {
             Female
         </label>
         <br></br>
-        <label>Enter the Blood type</label>
-        <input type="text" placeholder="Enter blood type" /><br></br>
+        <label>Enter parents or guardians name</label>
+        <input type="text" placeholder="Enter parents name"/><br></br>
+        <label>Enter phone number</label>
+        <input type="number" placeholder="Parent/guardian contact number"/><br></br>
+        <label>select registered hospital</label>
+        <select>
+          <option>Banadaragama hospital</option>
+          <option>Asiri hospital</option>
+
+        </select><br></br>
+        <label>Enter username</label>
+        <input type="text" placeholder="Enter username"/><br></br>
+        <label>Enter password</label>
+        <input type="password" placeholder="Enter password"/><br></br>
         
 
-
         
-
-        
-
-        
-
-
-
-
-
-
       </form>
       
     </div>

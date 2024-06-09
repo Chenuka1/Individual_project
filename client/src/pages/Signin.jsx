808da12b2fd519440e6445770db789cc63bd2392
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2024-06-09 12:59:31 +0530

    update signin.jsx

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -37,7 +37,7 @@ export default function Signin() {
             }
         } catch (error) {
             console.error('Error signing in:', error);
-            alert('.');
+            alert('Incorrect username and password.');
         }
     };
 
